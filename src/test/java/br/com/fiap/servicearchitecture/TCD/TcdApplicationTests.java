61a359ab1d981d15e54740acb82bdb69c806d278
FelipeLirao <felipe.lirao@hotmail.com>
2020-10-15 22:06:51 -0300

    Iniciando o projeto

---
+++ src/test/java/br/com/fiap/servicearchitecture/TCD/TcdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.servicearchitecture.TCD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
